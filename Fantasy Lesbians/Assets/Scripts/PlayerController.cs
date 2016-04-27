50046a09dc6d9bf74f1d7f8f807b4ab6c17acabb
PatchworkFox <delerioussheep@gmail.com>
2016-04-27 15:29:36 -0500

    got her moving again
    messing with lerp stuff made her immovable, still doesnt lerp but she
    does move

--- Fantasy Lesbians/Assets/Scripts/PlayerController.cs
+++ Fantasy Lesbians/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 pos;
     private Vector3 endpos;
+    private Vector2 input;
     private float gridSize = 64f;
     private float moveSpeed = 128f;
+    private bool allowDiagonals = false;
+    private bool correctDiagonalSpeed = true;
 
     bool lerp;
 
@@ -22,22 +25,19 @@ public class PlayerController : MonoBehaviour
 
     GridMove GM = new GridMove();
 
-	// Use this for initialization
+	
 	void Start () 
     {
         pos = transform.position;
-        //endpos = transform.position;
         lerp = false;
         
 	}
 	
-	// Update is called once per frame
 	void Update () 
     {
         if (lerp)
         {
-            GM = this.GetComponent<GridMove>();
-            GM.move(transform);
+            StartCoroutine(move(transform));
         }
 
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) && wallRight == false)
@@ -45,24 +45,53 @@ public class PlayerController : MonoBehaviour
             pos = transform.position;
             endpos = new Vector3(pos.x += gridSize, pos.y, 0);
             lerp = true;
+            input.y = 0;
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) && wallLeft == false)
         {
             pos = transform.position;
             endpos = new Vector3(pos.x -= gridSize, pos.y, 0);
             lerp = true;
+            input.y = 0;
         }
         else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && wallUp == false)
         {
             pos = transform.position;
             endpos = new Vector3(pos.x, pos.y += gridSize, 0);
             lerp = true;
+            input.x = 0;
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && wallDown == false)
         {
             pos = transform.position;
             endpos = new Vector3(pos.x, pos.y -= gridSize, 0);
             lerp = true;
+            input.x = 0;
         }
 	}
+
+    public IEnumerator move(Transform transform)
+    {
+        isMoving = true;
+        t = 0;
+
+        if (allowDiagonals && correctDiagonalSpeed && input.x != 0 && input.y != 0)
+        {
+            factor = 0.7071f;
+        }
+        else
+        {
+            factor = 1f;
+        }
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * (moveSpeed / gridSize) * factor;
+            transform.position = Vector3.Lerp(pos, endpos, t);
+            yield return null;
+        }
+
+        isMoving = false;
+        yield return 0;
+    }
 }
